fdbb03f0a82232cafbc42a7f5d72031359ba30e6
myCodingSpace <88979248+Alfiyaali@users.noreply.github.com>
2023-12-27 20:09:23 +0530

    new Update App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import ExpenseItem  from "./components/ExpenseItem";
 
 function App() {
+  const expense = [
+    {id: 'e1', expenseName: 'Food', price: 10},
+    {id: 'e2',expenseName: 'Petrol', price: 100},
+    {id: 'e3',expenseName: 'Movie', price: 200}
+  ]
   return (
     <div>
-      <ExpenseItem></ExpenseItem>
+    <h2 className='expense-heading'>Expense Item</h2>
+      <ExpenseItem 
+      expenseName={expense[0].expenseName} 
+      price={expense[0].price}>
+      </ExpenseItem>
+      
+      <ExpenseItem 
+      expenseName={expense[1].expenseName} 
+      price={expense[1].price}>
+      </ExpenseItem>
+
+      <ExpenseItem 
+      expenseName={expense[2].expenseName} 
+      price={expense[2].price}>
+      </ExpenseItem>
     </div>
   );
 }
