b13fc1493d8d249f18ab3868dc485e58126588f7
myCodingSpace <88979248+Alfiyaali@users.noreply.github.com>
2023-12-27 20:07:33 +0530

    new ExpenseItem commit

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,30 +1,10 @@
 import './ExpenseItem.css'
-function ExpenseItem(){
-    const expenseName1 = "Food";
-    const price1 = 10
-
-    const expenseName2 = "Petrol";
-    const price2 = 100
-
-    const expenseName3 = "Movie";
-    const price3 = 300
+function ExpenseItem(props){
     return(
         <div>
-            <h2 className='expense-heading'>Expense Items</h2>
-            
             <div className="expense-div">
-            <p className='expenseName'>{expenseName1}</p>
-            <p className='price'>Rs {price1}</p>
-            </div>
-            
-            <div className="expense-div">
-            <p className='expenseName'>{expenseName2}</p>
-            <p className='price'>Rs {price2}</p>
-            </div>
-            
-            <div className="expense-div">
-            <p className='expenseName'>{expenseName3}</p>
-            <p className='price'>Rs {price3}</p>
+            <p className='expenseName'>{props.expenseName}</p>
+            <p className='price'>Rs {props.price}</p>
             </div>
         </div>
     )
