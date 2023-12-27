08560e9f836c2129fec193263b63312b4f91600e
myCodingSpace <88979248+Alfiyaali@users.noreply.github.com>
2023-12-27 17:39:40 +0530

    Update ExpenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,31 @@
+import './ExpenseItem.css'
 function ExpenseItem(){
+    const expenseName1 = "Food";
+    const price1 = 10
+
+    const expenseName2 = "Petrol";
+    const price2 = 100
+
+    const expenseName3 = "Movie";
+    const price3 = 300
     return(
         <div>
-            <h2>Expense Items</h2>
-            <p>Food <span>Rs 10</span></p>
-            <p>Petrol <span>Rs 100</span></p>
-            <p>Movie <span>Rs 200</span></p>
+            <h2 className='expense-heading'>Expense Items</h2>
+            
+            <div className="expense-div">
+            <p className='expenseName'>{expenseName1}</p>
+            <p className='price'>Rs {price1}</p>
+            </div>
+            
+            <div className="expense-div">
+            <p className='expenseName'>{expenseName2}</p>
+            <p className='price'>Rs {price2}</p>
+            </div>
+            
+            <div className="expense-div">
+            <p className='expenseName'>{expenseName3}</p>
+            <p className='price'>Rs {price3}</p>
+            </div>
         </div>
     )
 }
