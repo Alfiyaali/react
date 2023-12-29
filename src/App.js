79add127180764dc65cf559e992e4fc3181ced33
myCodingSpace <88979248+Alfiyaali@users.noreply.github.com>
2023-12-29 20:23:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,27 +2,27 @@ import ExpenseItem  from "./components/ExpenseItem";
 
 function App() {
   const expense = [
-    {id: 'e1', expenseName: 'Food', price: 10},
-    {id: 'e2',expenseName: 'Petrol', price: 100},
-    {id: 'e3',expenseName: 'Movie', price: 200}
+    {id: 'e1', date: new Date(2023,5,12), expenseName: 'Food', price: 10},
+    {id: 'e2',date: new Date(2023,8,30),expenseName: 'Petrol', price: 100},
+    {id: 'e3',date: new Date(2023,10,27),expenseName: 'Movie', price: 200}
   ]
   return (
-    <div>
+    <div className="container">
     <h2 className='expense-heading'>Expense Item</h2>
       <ExpenseItem 
+      date = {expense[0].date}
       expenseName={expense[0].expenseName} 
-      price={expense[0].price}>
-      </ExpenseItem>
+      price={expense[0].price} />
       
       <ExpenseItem 
+      date = {expense[1].date}
       expenseName={expense[1].expenseName} 
-      price={expense[1].price}>
-      </ExpenseItem>
+      price={expense[1].price} />
 
       <ExpenseItem 
+      date = {expense[2].date}
       expenseName={expense[2].expenseName} 
-      price={expense[2].price}>
-      </ExpenseItem>
+      price={expense[2].price} />
     </div>
   );
 }
