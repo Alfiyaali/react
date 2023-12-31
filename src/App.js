5d983c88e9fac8eba116fdad6798a887e42b93a0
alfiyaali2601 <88979248+Alfiyaali@users.noreply.github.com>
2023-12-31 18:17:06 +0530

    all changes done

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
-import ExpenseItem  from "./components/ExpenseItem";
-
+import React from "react";
+import './index.css';
+import Expenses from "./components/Expenses/Expenses";
 function App() {
   const expense = [
-    {id: 'e1', date: new Date(2023,5,12), expenseName: 'Food', price: 10},
-    {id: 'e2',date: new Date(2023,8,30),expenseName: 'Petrol', price: 100},
-    {id: 'e3',date: new Date(2023,10,27),expenseName: 'Movie', price: 200}
+    {id: 'e1', date: new Date(2023,5,12), title: 'Food', amount: 10},
+    {id: 'e2',date: new Date(2023,8,30),title: 'Petrol', amount: 100},
+    {id: 'e3',date: new Date(2023,10,27),title: 'Movie', amount: 200}
   ]
-  return (
-    <div className="container">
-    <h2 className='expense-heading'>Expense Item</h2>
-      <ExpenseItem 
-      date = {expense[0].date}
-      expenseName={expense[0].expenseName} 
-      price={expense[0].price} />
-      
-      <ExpenseItem 
-      date = {expense[1].date}
-      expenseName={expense[1].expenseName} 
-      price={expense[1].price} />
 
-      <ExpenseItem 
-      date = {expense[2].date}
-      expenseName={expense[2].expenseName} 
-      price={expense[2].price} />
+  return (
+    <div>
+      <h2>Expense Tracker</h2>
+      <Expenses items={expense}/>
     </div>
   );
 }
