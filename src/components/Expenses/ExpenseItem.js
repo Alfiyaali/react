0f1a3d7976b5c0ba9a05e3be2ef8d766750a93a9
myCodingSpace <88979248+Alfiyaali@users.noreply.github.com>
2024-01-06 14:32:25 +0530

    kiy

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ExpenseDate from './ExpenseDate';
+import DeleteExpense from './DeleteExpense';
 import './ExpenseItem.css';
 import Card from '../UI/Card';
+
 function ExpenseItem(props){
+    const handleDeleteExpense = () => {
+        var list = document.querySelector(".expense-item")
+        list.remove();
+        console.log('Expense deleted!');
+      };
     return(
             <Card className="expense-item">
             <ExpenseDate date={props.date} />
                 <div className="expense-item_description">
                     <h2>{props.title}</h2>
                     <div className="expense-item_price">${props.amount}</div>
+                    <DeleteExpense onDelete={handleDeleteExpense} />
                 </div>
+   
             </Card>
     );
 }
