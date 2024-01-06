983f9a3f85c45b8bcc717ec533d9517ee6973eef
myCodingSpace <88979248+Alfiyaali@users.noreply.github.com>
2024-01-06 17:28:32 +0530

    first assignment

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import ExpenseDate from './ExpenseDate';
-import DeleteExpense from './DeleteExpense';
 import './ExpenseItem.css';
 import Card from '../UI/Card';
 
 function ExpenseItem(props){
-    const handleDeleteExpense = () => {
-        var list = document.querySelector(".expense-item")
-        list.remove();
-        console.log('Expense deleted!');
+    const deleteExpenseHandler = () => {
+        // DOM manipulation to remove the expense item
+        const expenseItemElement = document.querySelector('.expense-item');
+
+        // Remove the parent element from the DOM
+        expenseItemElement.remove();
       };
+    
     return(
-            <Card className="expense-item">
-            <ExpenseDate date={props.date} />
-                <div className="expense-item_description">
-                    <h2>{props.title}</h2>
-                    <div className="expense-item_price">${props.amount}</div>
-                    <DeleteExpense onDelete={handleDeleteExpense} />
-                </div>
-   
-            </Card>
+    <Card className="expense-item" id={props.id}>
+    <ExpenseDate date={props.date} />
+      <div className="expense-item_description">
+        <h2>{props.title}</h2>
+        <div className="expense-item_price">${props.amount}</div>
+        <button onClick={deleteExpenseHandler}>Delete Expense</button>
+      </div>
+    </Card>
     );
 }
 
