0d349a9860f0d9d89f9c80b60f89cdb3aede19b2
myCodingSpace <88979248+Alfiyaali@users.noreply.github.com>
2024-01-07 16:21:17 +0530

    form

---
+++ src/components/Expenses/ExpenseForm.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './ExpenseForm.css';
+
+function ExpenseForm() {
+  return (
+    <div>
+      <form>
+        <label htmlFor="">Expense Title</label>
+        <input type="text" name="title" id="title" />
+        <label htmlFor="">Expense Amount</label>
+        <input type="number" name="amount" id="amount" />
+        <label htmlFor="">Expense Date</label>
+        <input type="date" name="date" id="date" />
+      </form>
+    </div>
+  )
+}
+
+export default ExpenseForm
