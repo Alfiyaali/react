c05052b42cfaefd5da7a289d3ad7cf7f00f98cd2
myCodingSpace <88979248+Alfiyaali@users.noreply.github.com>
2024-01-07 16:53:18 +0530

    event add

--- src/components/Expenses/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import './ExpenseForm.css';
 
-function ExpenseForm() {
+const ExpenseForm = () => {
+    const formSubmit = (event) =>{
+        console.log(event.target.value)
+    };
   return (
     <div>
       <form>
         <label htmlFor="">Expense Title</label>
-        <input type="text" name="title" id="title" />
+        <input type="text" name="title" id="title" onChange={formSubmit} />
         <label htmlFor="">Expense Amount</label>
         <input type="number" name="amount" id="amount" />
         <label htmlFor="">Expense Date</label>
