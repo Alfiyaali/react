54d1e83e0c6306bb044ae46172d584b810c9e2f4
myCodingSpace <88979248+Alfiyaali@users.noreply.github.com>
2024-01-09 14:24:21 +0530

    Update App file

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
-import React from "react";
-import './index.css';
-import Expenses from "./components/Expenses/Expenses";
-import ExpenseForm from "./components/Expenses/ExpenseForm";
-function App() {
-  const expense = [
-    {id: 'e1', date: new Date(2023,5,12), title: 'Food', amount: 10},
-    {id: 'e2',date: new Date(2023,8,30),title: 'Petrol', amount: 100},
-    {id: 'e3',date: new Date(2023,10,27),title: 'Movie', amount: 200}
-  ]
+import React, { useState } from "react";
 
+import NewExpenses from "./components/NewExpenses/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  const addExpenseHandler = (expense) => {
+    // console.log(DUMMY_EXPENSES);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+    
+  };
+  
   return (
     <div>
-      <h2>Expense Tracker</h2>
-      <ExpenseForm />
-      <Expenses items={expense}/>
+      <NewExpenses onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
